3731a6632bfb657a7e0b412a465f1812bd650e46
Billy Shambrook <billy.shambrook@gmail.com>
2014-11-23 20:12:56 +0000

    Initial code commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.1.0'
+
+
+setup(
+    name='awsarchive',
+    version=VERSION,
+    author='Billy Shambrook',
+    description='Toolkit for archiving files using AWS products.',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    keywords="aws glacier s3 backup archive sqs",
+    packages=find_packages(exclude='docs'),
+    url="https://github.com/billyshambrook/awsarchive",
+    download_url="https://github.com/billyshambrook/awsarchive/tarball/{}".format(VERSION),
+    install_requires=['boto'],
+    entry_points={
+        'console_scripts': [
+            'awsarchive = awsarchive.__init__:main'
+        ]
+    }
+)
